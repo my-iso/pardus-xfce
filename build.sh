0e205ace8ba3d596a2146892519fb1684f32b33c
mobilturka <93243536+mobilturka@users.noreply.github.com>
2024-01-07 02:29:32 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,10 @@ set -ex
 mkdir chroot || true
 export DEBIAN_FRONTEND=noninteractive
 ln -s sid /usr/share/debootstrap/scripts/yirmiuc-deb || true
-debootstrap  --no-check-gpg --no-merged-usr --arch=i386 yirmiuc-deb chroot https://depo.pardus.org.tr/pardus
+debootstrap  --no-check-gpg --no-merged-usr --arch=x86_64 yirmiuc-deb chroot https://depo.pardus.org.tr/pardus
 for i in dev dev/pts proc sys; do mount -o bind /$i chroot/$i; done
 
 cat > chroot/etc/apt/sources.list.d/pardus.list << EOF
-deb http://depo.pardus.org.tr/pardus yirmibir main contrib non-free
-deb http://depo.pardus.org.tr/guvenlik yirmibir main contrib non-free
 deb http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free non-free-firmware
 deb http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free non-free-firmware
 #deb http://depo.pardus.org.tr/guvenlik yirmiuc main contrib non-free non-free-firmware
@@ -16,38 +14,24 @@ EOF
 chroot chroot apt-get update --allow-insecure-repositories
 chroot chroot apt-get install pardus-archive-keyring --allow-unauthenticated -y
 
+cat > chroot/etc/apt/sources.list.d/yirmiuc-backports.list << EOF
+deb http://depo.pardus.org.tr/backports yirmiuc-backports main contrib non-free non-free-firmware
+EOF
+
 chroot chroot apt-get update -y
 chroot chroot apt-get install gnupg -y
 
-chroot chroot apt-get install grub-pc-bin grub-efi-ia32 -y
+chroot chroot apt-get install grub-pc-bin grub-efi-ia32-bin grub-efi -y
 chroot chroot apt-get install live-config live-boot plymouth plymouth-themes -y
 
 echo -e "#!/bin/sh\nexit 101" > chroot/usr/sbin/policy-rc.d
 chmod +x chroot/usr/sbin/policy-rc.d
 
-#cat > chroot/etc/apt/sources.list.d/devuan.list << EOF
-#deb http://deb.devuan.org/merged stable main contrib non-free non-free-firmware
-#deb http://deb.devuan.org/merged stable-updates main contrib non-free non-free-firmware
-#deb http://deb.devuan.org/merged stable-security main contrib non-free non-free-firmware
-#deb http://deb.devuan.org/merged stable-backports main contrib non-free non-free-firmware
-#EOF
-
-#rm -f chroot/var/lib/dpkg/info/systemd.prerm
-#chroot chroot apt-get update --allow-insecure-repositories
-#chroot chroot apt-get install devuan-keyring --allow-unauthenticated -y
-#chroot chroot apt-get update
-#chroot chroot apt install elogind eudev sysvinit-core sysv-rc ntp -y --allow-remove-essential -y
-#chroot chroot apt-get full-upgrade -y
-#chroot chroot apt-get autoremove --purge -y
-
 # xorg & desktop pkgs
 chroot chroot apt-get install lsb-release pavucontrol xserver-xorg pipewire xinit lightdm pardus-lightdm-greeter network-manager-gnome -y
 chroot chroot apt-get install pardus-installer -y
-chroot chroot apt-get install lxde lxde-core --no-install-recommends -y
+chroot chroot apt-get install cinnamon --no-install-recommends -y
 
-#wget -O chroot/tmp/17g.deb https://github.com/PuffOS/17g-installer/releases/download/current/17g-installer_1.0_all.deb
-#chroot chroot dpkg -i /tmp/17g.deb || true
-#chroot chroot apt install -f -y
 
 #### Remove bloat files after dpkg invoke (optional)
 cat > chroot/etc/apt/apt.conf.d/02antibloat << EOF
@@ -59,8 +43,8 @@ DPkg::Post-Invoke {"rm -rf /usr/share/info || true";};
 EOF
 
 chroot chroot apt-get update -y
-chroot chroot apt-get install linux-image-686-pae -y
-chroot chroot apt-get install -y firmware-amd-graphics firmware-atheros \
+chroot chroot apt-get install -t yirmiuc-backports linux-image-amd64 -y
+chroot chroot apt-get install -t yirmiuc-backports -y firmware-amd-graphics firmware-atheros \
     firmware-b43-installer firmware-b43legacy-installer \
     firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-linux-free \
     firmware-cavium firmware-intel-sound firmware-intelwimax \
@@ -88,7 +72,7 @@ cp -pf chroot/boot/vmlinuz-* pardus/live/vmlinuz
 
 mkdir -p pardus/boot/grub/
 echo 'terminal_output console' > pardus/boot/grub/grub.cfg
-echo 'menuentry "Start Pardus GNU/Linux LXDE 32-bit (Unofficial)" --class pardus {' >> pardus/boot/grub/grub.cfg
+echo 'menuentry "Start Pardus GNU/Linux Cinnamon (Unofficial)" --class pardus {' >> pardus/boot/grub/grub.cfg
 echo '    linux /live/vmlinuz boot=live components --' >> pardus/boot/grub/grub.cfg
 echo '    initrd /live/initrd.img' >> pardus/boot/grub/grub.cfg
 echo '}' >> pardus/boot/grub/grub.cfg
